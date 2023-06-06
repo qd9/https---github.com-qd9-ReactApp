f6f262da7c1d304c9c56531d63cde11f28b8a988
qd9 <qd9@njit.edu>
2023-06-06 01:34:33 -0400

    React App

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import Expenses from "./components/Expenses";
+
+function App() {
+  const expenses = [
+    { title: 'Title 1', amount: '11111', date: new Date() },
+    { title: 'Title 2', amount: '111222', date: new Date() },
+  ]
+
+  return (
+    <div>
+      <h2>Let's get started!</h2>
+      <h1>Sample</h1>
+      <Expenses items={expenses} />
+    </div>
+
+  );
+}
+
+export default App;
