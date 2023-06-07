ee0f530231490add3075d7b0011565ba746f80d3
qd9 <qd9@njit.edu>
2023-06-07 01:45:49 -0400

    add errow function.

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import Expenses from "./components/Expenses/Expenses";
 
-function App() {
+const App = () => {
   const expenses = [
     { title: 'Title 1', amount: '11111', date: new Date() },
     { title: 'Title 2', amount: '111222', date: new Date() },
