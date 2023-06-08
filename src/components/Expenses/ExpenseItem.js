c39866f9fcf67d3aff00bcaa77c03e53f6af4bd5
qd9 <qd9@njit.edu>
2023-06-08 02:31:28 -0400

    add state into code

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -1,14 +1,22 @@
+import React, { useState } from 'react';
 import ExpenseDate from './ExpenseDate';
 import Card from '../UI/Card';
 import './ExpenseItem.css';
 
 const ExpenseItem = (props) => {
+    //useState always return an array, first element is the state value and second value is the function.
+    const [title, setTitle] = useState(props.title);
+
+    const clickHandler = () => {
+        setTitle('UPDATED!');
+    };
 
     return (
         <Card className='expense-item'>
-            <ExpenseDate date = {props.date}/>
-            <div className='expense-item__description'><h2>{props.title}</h2></div>
+            <ExpenseDate date={props.date} />
+            <div className='expense-item__description'><h2>{title}</h2></div>
             <div className='expense-item__price'>${props.amount}</div>
+            <button onClick={clickHandler}>Change Title</button>
         </Card>
     );
 }
