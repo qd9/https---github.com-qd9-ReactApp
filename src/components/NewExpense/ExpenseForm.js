e3bdcf0b79fe951a5fed84521c6783e3b45c0ab4
qd9 <qd9@njit.edu>
2023-06-10 02:25:37 -0400

    add form submition

--- src/components/NewExpense/ExpenseForm.js
+++ src/components/NewExpense/ExpenseForm.js
@@ -13,7 +13,7 @@ const ExpenseForm = () => {
     });
 
     const titleChangeHandler = (event)=> {
-        console.log(event);
+        //console.log(event);
         //setEnteredTitle(event.target.value);
         // setUserInput({
         //     ...userInput,
@@ -30,7 +30,7 @@ const ExpenseForm = () => {
     };
 
     const amountChangeHandler = event =>{
-        console.log(event.target.value);
+        //console.log(event.target.value);
         //setEnteredAmount(event.target.value);
         // setUserInput({
         //     ...userInput,
@@ -45,7 +45,7 @@ const ExpenseForm = () => {
     };
 
     const dateChangeHandler = event => {
-        console.log(event.target.value);
+        //console.log(event.target.value);
         //setEnteredDate(event.target.value);
         // setUserInput({
         //     ...userInput,
@@ -59,24 +59,42 @@ const ExpenseForm = () => {
         })
     };
 
+    const submitHandler = (event) =>{
+        event.preventDefault();
+
+        const expenseData = {
+            title:userInput.enteredTitle,
+            amount:userInput.enteredAmount,
+            date:new Date(userInput.enteredDate)
+        }
+        console.log(expenseData);
+        setUserInput(()=>{
+            return{
+                enteredTitle:'',
+                enteredAmount:'',
+                enteredDate:''
+            }
+        })
+    };
+
     return (
-        <form>
+        <form onSubmit={submitHandler}>
             <div className="new-expense__controls">
                 <div className="new-expense__control">
                     <label>Title</label>
-                    <input type="text" onChange={titleChangeHandler}/>
+                    <input type="text" value={userInput.enteredTitle} onChange={titleChangeHandler}/>
                 </div>
                 <div className="new-expense__control">
                     <label>Amount</label>
-                    <input type="number" min="0.01" step="0.01" onChange={amountChangeHandler} />
+                    <input type="number" min="0.01" step="0.01" value = {userInput.enteredAmount} onChange={amountChangeHandler} />
                 </div>
                 <div className="new-expense__control">
                     <label>Date</label>
-                    <input type="date" min="2019-01-01" onChange={dateChangeHandler}/>
+                    <input type="date" min="2019-01-01" value={userInput.enteredDate} onChange={dateChangeHandler}/>
                 </div>
             </div>
             <div className="new-expense__actions">
-                <button type="submit">Add Expense</button>
+                <button type="submit" >Add Expense</button>
             </div>
         </form>
     )
