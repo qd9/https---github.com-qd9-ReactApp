1b603c41e8ea23aa740229a811f9856261de1e1c
qd9 <qd9@njit.edu>
2023-06-15 02:26:31 -0400

    add condition

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -18,20 +18,35 @@ const Expenses = (props) => {
         return expense.date.getFullYear().toString() === filteredYear;
     });
 
+    let expensesContent = <p>No Expense Found! </p>;
+    if (filteredExpenses.length > 0) {
+        expensesContent = filteredExpenses.map((expense, index) => (
+            <ExpenseItem 
+                key = {expense.id}
+                title={expense.title} 
+                amount={expense.amount} 
+                date={expense.date}   
+            />
+        ))
+    };
+
     return (
         <div>
             <Card className='expenses'>
                 <ExpensesFilter selected = {filteredYear} onChangeFilter = {filterChangeHandler}></ExpensesFilter>
                 {/* Key can be any unique ID */}
-                {filteredExpenses.map((expense, index) => (
-                    <ExpenseItem 
-                        key = {expense.id}
-                        title={expense.title} 
-                        amount={expense.amount} 
-                        date={expense.date}
-                        
-                    />
-                ))}         
+                {/* {filteredExpenses.length === 0 && <p>No Expense Found! </p>}
+                {filteredExpenses.length > 0 &&
+                    filteredExpenses.map((expense, index) => (
+                        <ExpenseItem 
+                            key = {expense.id}
+                            title={expense.title} 
+                            amount={expense.amount} 
+                            date={expense.date}   
+                        />
+                    ))
+                }      */}
+                {expensesContent}
             </Card>
         </div>
     )
