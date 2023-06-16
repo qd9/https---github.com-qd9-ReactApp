c0e5f4289e4025f14919159f1328a8250cbfc193
qd9 <qd9@njit.edu>
2023-06-16 02:33:45 -0400

    fix bug in expsenseForm

--- src/components/NewExpense/ExpenseForm.js
+++ src/components/NewExpense/ExpenseForm.js
@@ -66,7 +66,7 @@ const ExpenseForm = (props) => {
 
         const expenseData = {
             title:userInput.enteredTitle,
-            amount:userInput.enteredAmount,
+            amount:+userInput.enteredAmount,
             date:new Date(userInput.enteredDate)
         }
         console.log(expenseData + 'form');
